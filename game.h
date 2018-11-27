dc166a2fdc2134e01ad3ecfa11217098c3374240
syahruddin <syahruddinr@gmail.com>
2018-11-27 18:31:59 +0700

    game.h sementara (belum selesai)

---
+++ game.h
@@ -0,0 +1,30 @@
+
+#ifndef GAME_H
+#define GAME_H
+
+
+#include "boolean.h"
+#include "jam.h"
+#include "listlinier.h"
+#include "matriks.h"
+#include "mesin_kar.h"
+#include "mesin_kata.h"
+#include "point.h"
+#include "stacklist.h"
+#include <stdio.h>
+
+
+void startProgram();
+
+void startGame();
+
+void getMapFile();
+
+void drawgame();
+
+void menu();
+
+void updateMap();
+
+
+#endif
