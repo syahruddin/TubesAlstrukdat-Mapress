1ee4c4f9bc2204b6742068e831c070aaf76f1d23
Sulthanm2d <sulthanmahdi7@gmail.com>
2018-11-29 05:24:08 +0700

    page

---
+++ page.h
@@ -0,0 +1,33 @@
+#ifndef PAGE_H
+#define PAGE_H
+#include <stdio.h>
+#include <stdlib.h>
+
+void CoverPage();
+// Menampilkan "Diner Dash" wkwkwkw
+
+void FrontPage();
+// Tampilan awal pada game
+
+void StartGame();
+// Memulai game (membaca map)
+
+void LoadGame();
+// Memulai game yang telah tersave (membaca map)
+
+void BacaMap();
+// Membaca Map
+
+void TulisMap();
+// Menampilkan Map
+
+void Quit();
+// Keluar dari game
+
+void GameOver();
+// Menampilkan Tulisan Game Over
+
+void Credit();
+// Menampilkan credit created by:
+
+#endif
