1c161ec569b28d2477ce9aef9c9898ae4ebe7699
Sulthanm2d <sulthanmahdi7@gmail.com>
2018-11-29 22:01:55 +0700

    page
    udah aku ganti Quit() jadi Exit(). ntar waktu masukin command EXIT keluarin Exit(); aja

--- main.c
+++ main.c
@@ -1,13 +1,6 @@
 #include "page.h"
-#include "game.h"
 
-char Map;
-char name;
-char menu;
 
 int main(){
-	Map = "rTamu.txt"; //bukan membaca, cuma kondisi awal aja
-	name[1] = ' '; //bukan membaca, cuma kondisi awal aja
-	menu = '0'; //bukan membaca, cuma kondisi awal aja
 	CoverPage();
 }
